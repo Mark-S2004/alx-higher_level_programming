4fe9bf09b618471fc6cfb29d31ac61b9bde552f4
Mark Saleh <mark-saleh@outlook.com>
2023-03-09 03:25:08 -0800

    Task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv, exit
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    operation = {'+': a+b, '-': a-b, '*': a*b, '/': a/b}
+    result = operation.get(argv[2], None)
+    if not result:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print(f"{a} {argv[2]} {b} = {result}")
