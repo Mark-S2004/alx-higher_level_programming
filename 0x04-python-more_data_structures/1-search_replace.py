8269860b5a06f60fa5abf520661fbab58a232169
Mark Saleh <mark-saleh@outlook.com>
2023-03-18 01:28:43 -0700

    Comply python file to PEP8 style Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	return list(map(lambda element: replace if element == search else element, my_list))
+    return list(map(
+                lambda element: replace if element == search else element,
+                my_list))
