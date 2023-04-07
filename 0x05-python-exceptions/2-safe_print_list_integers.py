a866472d677e430414fe46abcae11b3d99e38c4c
Mark Saleh <mark-saleh@outlook.com>
2023-04-07 15:11:29 +0000

    Task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count_printed = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+        except IndexError:
+            break
+        except (ValueError, TypeError):
+            continue
+        else:
+            count_printed += 1
+    print()
+    return count_printed
