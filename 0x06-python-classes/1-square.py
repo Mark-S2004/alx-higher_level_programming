3668c875907b4692dcb8525cce823d9308faa48b
Mark Saleh <mark-saleh@outlook.com>
2023-04-09 17:10:10 +0000

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Design and Build a Square class."""
+
+
+class Square:
+    """Sqaure Object.
+
+    Attributes:
+        size (int): Square size.
+
+    """
+
+    def __init__(self, size):
+        """Initialize Square Object instance.
+
+        Args:
+            size(int): Square size.
+        """
+        self.__size = size
