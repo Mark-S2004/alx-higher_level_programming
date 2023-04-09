bb0d51ee0fc4ac8d38d63f8faa049f467a473c4e
Mark Saleh <mark-saleh@outlook.com>
2023-04-09 18:10:59 +0000

    Task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Design and Build a Square class."""
+
+
+class Square:
+    """Sqaure Object.
+
+    Attributes:
+        size (int): Square size.
+
+    """
+
+    def __init__(self, size=0):
+        """Initialize Square Object instance.
+
+        Args:
+            size(int): Square size.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Get size attribute value."""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate current square area.
+
+        Returns:
+            area (int): Sqaure area
+
+        """
+        return self.__size**2
